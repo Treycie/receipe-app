ef49ad70cdb6fe29537dbc3a7a85e04c79bd7446
Trey_cie <kusiwaahboteng1997@gmail.com>
2024-02-26 08:05:03 -0800

    26/02/2024

--- src/pages/receipes/index.jsx
+++ src/pages/receipes/index.jsx
@@ -6,10 +6,32 @@ import {
   Grid,
   TextField,
   Typography,
+  CardActionArea,
 } from "@mui/material";
+import { useEffect, useState } from "react";
+
 export default function Receipes() {
+  const [receipes, setReceipes] = useState([]);
+  const getReceipes = () => {
+    // prepare URL
+    const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+    url.searchParams.append("apiKey", "42df0330e8074c8ab415d2b8fc98f505");
+    // fetch receipes from API
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // update the receipes state
+        setReceipes(data.results);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(getReceipes, []);
+
   return (
-    <Container sx={{ my: "2rem" }} max-width="sm">
+    <Container sx={{ my: "2rem" }}>
       <TextField
         fullWidth
         id="outlined-basic"
@@ -18,17 +40,29 @@ export default function Receipes() {
       />
 
       <Grid sx={{ mt: "1rem" }} container spacing={3}>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://media.istockphoto.com/id/1462751830/photo/stewed-cod.jpg?s=1024x1024&w=is&k=20&c=4BFA7UeFG2yjrqbaRK9yQKx3M6OWC3dEuS4_MirblTU="
-            />
-          </Card>
-          <CardContent>
-            <Typography variant="h5">Receipe Name</Typography>
-          </CardContent>
-        </Grid>
+        {receipes.map(receipe => (<Grid key={receipe.id} item
+           item xs={4}>
+            <Card sx={{ maxWidth: 345,height:'100%' }}>
+              <CardActionArea sx ={{height:'100%'}}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={receipe.image}
+                  alt={receipe.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {receipe.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  ></Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
