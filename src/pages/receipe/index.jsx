b20cf4e504fe8fd0a95cd1bf0ce6697081458a86
Trey_cie <kusiwaahboteng1997@gmail.com>
2024-02-29 08:50:04 -0800

    29/02/2024

--- src/pages/receipe/index.jsx
+++ src/pages/receipe/index.jsx
@@ -1,7 +1,38 @@
-import { useParams } from "react-router-dom";
+import {Link, useParams } from "react-router-dom";
+import useSWR from "swr"
+import loadingIcon from "../../assets/images/loadingIcon.svg";
+import {Container} from "@mui/material"
+
+
+const getReceipe =(...args) => {
+ // prepare URL
+ const url = new URL(args[0]);
+ url.searchParams.append("apiKey",process.env.REACT_APP_SPOONACULAR_API_KEY)
+//  fetch and return receipe
+return fetch(url).then(response => response.json());
+}
+
+ 
 
 export default function Receipe() {
   const { id } = useParams();
+  const {data,isLoading, } = useSWR(`https://api.spoonacular.com/recipes/${id}/information`, getReceipe);
+  // console.log(data, isLoading);
 
-  return <h1>Receipe Page: {id}</h1>;
+
+
+  return (
+    <>
+    {isLoading ? <img src={{loadingIcon}} /> :(
+      <Container>
+      <h1>{data.title}</h1>
+      <div>{data.summary}</div>
+      <div dangerouslySetInnerHTML={{__html: data.
+        summary}} />
+      <img src={data.image} />
+      </Container>
+    )}
+    </>
+  );
+  
 }
